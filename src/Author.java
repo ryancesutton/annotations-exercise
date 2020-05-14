e209ce3f290d6c5eca762c201109d39cc523d1ac
Ry Sutton <ryancesutton@gmail.com>
2020-05-14 14:03:29 -0500

    complete first part of exercise

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings({"unchecked", "overrides"})
 public class Author extends Person {
     private List books;
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+
+@SuppressWarnings({"deprecation","overrides"})
 public class Main {
+
     public static void main(String[] args) {
+
         Author author = new Author("Sandi", "Metz");
 
         author.addBook("Practical Object-Oriented Design in Ruby");
